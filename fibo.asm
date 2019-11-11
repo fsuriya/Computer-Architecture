99cf0880b2aa8d522326f5273214c900ed39511f
fsuriya <flukbanrong@gmail.com>
2019-11-11 19:45:15 +0700

    Update fibo.asm

--- fibo.asm
+++ fibo.asm
@@ -16,20 +16,30 @@ int main(int n){
 }
 
 $1 = 0      #temp1
-$2 = 1      #temp2
+$2 = 1      #temp2,return
 $3          #temp3
 $4 = n      #input
 $5 = 1      #i
 $6 = 1
-
-        beq     1       4       done
-        beq     2       4       done
-L1:     add     0       2       3
+        sw      0       1       temp1
+        sw      0       2       temp2
+        sw      0       4       n
+        sw      0       5       i
+        sw      0       6       one
+        beq     1       4       done1
+        beq     2       4       done2
+L1      add     0       2       3
         add     1       2       2
         add     0       3       1
         add     5       6       5
         beq     5       4       L1
-done    noop
-end     .fill
-L1      .fill
-        
+done1   add     0       1       2 
+done2   noop
+done1   .fill   done1
+done2   .fill   done2
+L1      .fill   L1
+temp1   .fill   0
+temp2   .fill   1
+n       .fill   input_Argc        
+if      .fill   1
+one     .fill   1
