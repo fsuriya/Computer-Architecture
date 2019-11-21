43a28008b92463706370533ed40a2cfb75c80487
fsuriya <35364690+fsuriya@users.noreply.github.com>
2019-11-21 21:00:38 +0700

    Update fibo.asm

--- fibo.asm
+++ fibo.asm
@@ -26,20 +26,20 @@ $6 = 1
         lw      0       4       n
         lw      0       5       i
         lw      0       6       one
-        beq     1       4       done1
-        beq     2       4       done2
+        beq     1       4       done11
+        beq     2       4       done21
 L1      add     0       2       3
         add     1       2       2
         add     0       3       1
         add     5       6       5
-        beq     5       4       L1
+        beq     5       4       L11
 done1   add     0       1       2 
-done2   noop
-done1   .fill   done1
-done2   .fill   done2
-L1      .fill   L1
+done2   halt
+done11   .fill   done1
+done22   .fill   done2
+L11      .fill   L1
 temp1   .fill   0
 temp2   .fill   1
 n       .fill   input_Argc        
-if      .fill   1
+i       .fill   1
 one     .fill   1
