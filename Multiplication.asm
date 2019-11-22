42ccc8f68eeda7d05e023d33c044c0d7ec7324ff
fsuriya <35364690+fsuriya@users.noreply.github.com>
2019-11-22 15:11:24 +0700

    Update Multiplication.asm

--- Multiplication.asm
+++ Multiplication.asm
@@ -7,30 +7,34 @@ $5 = 32 endloop
 $6 selectbit
 $7 = 1 constant
 
-				lw		0		2		Mcand
-				lw		0		3		Mplier
-				lw		0		5		endloop
-				lw		0		6		selectbit
-				lw		0		7		one
-				sw		7		4		storecountloop
-selMplier		        nand	        3		6		4                               #selectMplier to temp
-                                nand            4               4               4
-				beq		0		4		shMcand
-				add		2		1		1				#add Mcand to ans($1)
-shMcand			        add		2		2		2				#shift Mcand
-				add		6		6		6				#shift selectbit
-				lw		7		4		storecountloop
-				add		7		4		4
-				sw		7		4		storecountloop
-				beq		5		4		done				#if bit 32 goto done
-				beq		0		0		selMplier
-done			       	halt
-Mcand		 .fill		input1
-Mplier		 .fill		input2
-selectbit	 .fill		1
-endloop		 .fill		32
-one		 .fill		1
-shiftMcand	 .fill		shMcand
-done1		 .fill		done
-selectMplier     .fill		selMplier
-storecountloop   .fill          30
+				lw		0		2		Mcand			#$2 = Mcand
+				lw		0		3		Mplier			#$3 = Mplier
+				lw		0		5		endloop			#$5 = endloop
+				lw		0		6		selectbit		#$6 = selectbit
+				lw		0		7		one			#$7 = constant value 1
+				sw		7		4		loopcount		#initailize countloop
+				lw		0		4		blockoverflow		
+				sw		7		4		memoverflow		#constant overflow value 
+selMplier			nand		3		6		4               	#selectMplier to temp
+				nand		4		4		4			#true selectMplier
+				beq		0		4		shMcand			#if selectMplier == 0 skip add
+				add		2		1		1			#add Mcand to answer($1)
+shMcand				add		2		2		2			#shift Mcand
+				lw		7		4		memoverflow		#load memory overflow to $4
+				nand		2		4		4			#fillterdata
+				nand		4		4		2			#true fillterdata
+				add		6		6		6			#shift selectbit
+				lw		7		4		loopcount		#load memory loopcount
+				add		7		4		4			#loopcount++
+				sw		7		4		loopcount		#store loopcount to memory
+				beq		5		4		done			#if loopcount == 32 goto done
+				beq		0		0		selMplier		#goto selMplier
+done				halt									#end program
+Mcand		 	.fill		32766
+Mplier		 	.fill		10383
+selectbit	 	.fill		1
+endloop			.fill		32
+one		 	.fill		1
+loopcount	    	.fill	       	30
+memoverflow		.fill		32
+blockoverflow		.fill		2147483647
